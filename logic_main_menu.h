8fe32d034042c8e72dbe2c78038d638e23bb0c4a
Hildemberg Eling <hildembergeling@gmail.com>
2024-03-19 10:54:37 -0300

    set to folders

--- logic_main_menu.h
+++
@@ -1,9 +0,0 @@
-#ifndef logic_main_menu_H
-#define logic_main_menu_H
-#include <stdio.h>
-#include <stdlib.h>
-#include "screens.h"
-#include "logic_login.h"
-void logic_main_menu(char option);
-char login_screen(void);
-#endif
