5691ee6556733ff8ff0d72caf8a09639818e5713
Hildemberg Eling <hildembergeling@gmail.com>
2024-03-20 08:15:22 -0300

    Creating reservation navigation screens

---
+++ screens/reservation_screens.c
@@ -0,0 +1,135 @@
+#include "reservation_screens.h"
+
+char reservation_menu(void){
+    char option;
+    system("clear||cls");
+    printf("\n");
+    printf("###############################################################################\n");
+    printf("###                                                                         ###\n");
+    printf("###             Universidade Federal do Rio Grande do Norte                 ###\n");
+    printf("###                 Centro de Ensino Superior do Seridó                     ###\n");
+    printf("###               Departamento de Computação e Tecnologia                   ###\n");
+    printf("###                  Disciplina DCT1106 -- Programação                      ###\n");
+    printf("###             Projeto Um Sistema de Gestão para um Hotel                  ###\n");
+    printf("###    Developed by https://github.com/Hildemberg986 -- since Mar, 2024     ###\n");
+    printf("###                                                                         ###\n");
+    printf("###############################################################################\n");
+    printf("###                                                                         ###\n");
+    printf("###                     |=====- Menu  Reservas -=====|                      ###\n");
+    printf("###                                                                         ###\n");
+    printf("###                          1-> Cadastrar Rerserva                         ###\n");
+    printf("###                          2-> Editar Rerserva                            ###\n");
+    printf("###                          3-> Excluir Rerserva                           ###\n");
+    printf("###                          4-> Pesquisar Rerserva                         ###\n");
+    printf("###                          0-> Sair                                       ###\n");
+    printf("###                                                                         ###\n");
+    printf("###############################################################################\n");
+    printf("\n");
+    printf("###  Digite a opção desejada e Tecle <ENTER> para continuar... ");
+    scanf(" %c", &option);
+    getchar();
+    return option;
+}
+
+void register_reservation_screen(void)
+{
+    struct register_reservation reservation;
+
+    system("clear||cls");
+    printf("\n");
+    printf("###############################################################################\n");
+    printf("###                                                                         ###\n");
+    printf("###             Universidade Federal do Rio Grande do Norte                 ###\n");
+    printf("###                 Centro de Ensino Superior do Seridó                     ###\n");
+    printf("###               Departamento de Computação e Tecnologia                   ###\n");
+    printf("###                  Disciplina DCT1106 -- Programação                      ###\n");
+    printf("###             Projeto Um Sistema de Gestão para um Hotel                  ###\n");
+    printf("###    Developed by https://github.com/Hildemberg986 -- since Mar, 2024     ###\n");
+    printf("###                                                                         ###\n");
+    printf("###############################################################################\n");
+    printf("###                   |=====- Cadastro de Reserva -=====|                   ###\n");
+    printf("###                                                                         ###\n");
+    printf("###   --> Digite o Numero do Quarto a Ser Reservado... ");
+    scanf("%99[^\n]", reservation.number);
+    getchar();
+    printf("###   --> Digite o Dia de Entrada... ");
+    scanf("%99[^\n]", reservation.day_enter);
+    getchar();
+    printf("###   --> Digite o Dia de Saida... ");
+    scanf("%99[^\n]", reservation.day_exit);
+    getchar();
+    printf("###   --> Digite o CPF do Cliente Que Reservou... ");
+    scanf("%99[^\n]", reservation.client_cpf);
+    getchar();
+    printf("\n");
+}
+
+void edit_reservation_screen(void)
+{
+    struct register_reservation reservation;
+
+    system("clear||cls");
+    printf("\n");
+    printf("###############################################################################\n");
+    printf("###                                                                         ###\n");
+    printf("###             Universidade Federal do Rio Grande do Norte                 ###\n");
+    printf("###                 Centro de Ensino Superior do Seridó                     ###\n");
+    printf("###               Departamento de Computação e Tecnologia                   ###\n");
+    printf("###                  Disciplina DCT1106 -- Programação                      ###\n");
+    printf("###             Projeto Um Sistema de Gestão para um Hotel                  ###\n");
+    printf("###    Developed by https://github.com/Hildemberg986 -- since Mar, 2024     ###\n");
+    printf("###                                                                         ###\n");
+    printf("###############################################################################\n");
+    printf("###                                                                         ###\n");
+    printf("###                     |=====- Editar Reserva -=====|                      ###\n");
+    printf("###                                                                         ###\n");
+    printf("###   --> Digite o CPF do Cliente Para Ver as Reservas... ");
+    scanf("%12[^\n]", reservation.client_cpf);
+    getchar();
+}
+void delete_reservation_screen(void)
+{
+    struct register_reservation reservation;
+
+    system("clear||cls");
+    printf("\n");
+    printf("###############################################################################\n");
+    printf("###                                                                         ###\n");
+    printf("###             Universidade Federal do Rio Grande do Norte                 ###\n");
+    printf("###                 Centro de Ensino Superior do Seridó                     ###\n");
+    printf("###               Departamento de Computação e Tecnologia                   ###\n");
+    printf("###                  Disciplina DCT1106 -- Programação                      ###\n");
+    printf("###             Projeto Um Sistema de Gestão para um Hotel                  ###\n");
+    printf("###    Developed by https://github.com/Hildemberg986 -- since Mar, 2024     ###\n");
+    printf("###                                                                         ###\n");
+    printf("###############################################################################\n");
+    printf("###                                                                         ###\n");
+    printf("###                     |=====- Excluir Reserva -=====|                     ###\n");
+    printf("###                                                                         ###\n");
+    printf("###   --> Digite o CPF do Cliente Para Ver as Reservas... ");
+    scanf("%12[^\n]", reservation.client_cpf);
+    getchar();
+}
+void read_reservation_screen(void)
+{
+    struct register_reservation reservation;
+
+    system("clear||cls");
+    printf("\n");
+    printf("###############################################################################\n");
+    printf("###                                                                         ###\n");
+    printf("###             Universidade Federal do Rio Grande do Norte                 ###\n");
+    printf("###                 Centro de Ensino Superior do Seridó                     ###\n");
+    printf("###               Departamento de Computação e Tecnologia                   ###\n");
+    printf("###                  Disciplina DCT1106 -- Programação                      ###\n");
+    printf("###             Projeto Um Sistema de Gestão para um Hotel                  ###\n");
+    printf("###    Developed by https://github.com/Hildemberg986 -- since Mar, 2024     ###\n");
+    printf("###                                                                         ###\n");
+    printf("###############################################################################\n");
+    printf("###                                                                         ###\n");
+    printf("###                    |=====- Pesquisar Reserva -=====|                    ###\n");
+    printf("###                                                                         ###\n");
+    printf("###   --> Digite o Numero do Quarto para Pesquisar as Reservas... ");
+    scanf("%12[^\n]", reservation.number);
+    getchar();
+}
---
+++ screens/reservation_screens.h
@@ -0,0 +1,17 @@
+#ifndef roons_screens_H
+#define roons_screens_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+void register_reservation_screen(void);
+void edit_reservation_screen(void);
+void delete_reservation_screen(void);
+void read_reservation_screen(void);
+struct register_reservation
+{
+    char number[99];
+    char day_enter[99];
+    char day_exit[99];
+    char client_cpf[12];
+};
+#endif
