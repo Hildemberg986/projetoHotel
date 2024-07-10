cd3a9988507597d234b893a215c09d0164f8ff2e
Hildemberg Eling <hildembergeling@gmail.com>
2024-07-10 08:03:25 -0300

    Modified the search functions and created other functions

--- function/database_functions.c
+++ function/database_functions.c
@@ -2,14 +2,14 @@
 
 void save_file_client(Client new_client)
 {
-    //Abre o arquivo e define um ponteiro para o mesmo
+    // Abre o arquivo e define um ponteiro para o mesmo
     FILE *arquivo = fopen("clientes.db", "ab");
 
     if (arquivo != NULL)
     {
-        //Escreve o valor new_client do tamanho do tipo Client uma vez no arquivo
+        // Escreve o valor new_client do tamanho do tipo Client uma vez no arquivo
         fwrite(&new_client, sizeof(Client), 1, arquivo);
-        //Fecha o arquivo aberto
+        // Fecha o arquivo aberto
         fclose(arquivo);
     }
     else
@@ -19,11 +19,11 @@ void save_file_client(Client new_client)
 }
 void load_file_client()
 {
-    //Abre o arquivo e define um ponteiro para o mesmo
-    FILE *arquivo = fopen("clientes.db", "rb"); 
+    // Abre o arquivo e define um ponteiro para o mesmo
+    FILE *arquivo = fopen("clientes.db", "rb");
     if (arquivo != NULL)
     {
-        //Cria uma variavel temporaria do tipo Client, um struct relacional 
+        // Cria uma variavel temporaria do tipo Client, um struct relacional
         Client loaded_client;
 
         // Lê enquanto houver clientes no arquivo
@@ -35,7 +35,7 @@ void load_file_client()
             printf("Telefone: %s\n", loaded_client.fone);
             printf("CPF: %s\n", loaded_client.cpf);
         }
-        //Fecha o arquivo aberto
+        // Fecha o arquivo aberto
         fclose(arquivo);
     }
     else
@@ -44,43 +44,30 @@ void load_file_client()
     }
 }
 
-const char *search_client_cpf(const char *cpf, bool silentSearch)
+Client *search_client_cpf(const char *cpf)
 {
     FILE *arquivo = fopen("clientes.db", "rb"); // Abre o arquivo para leitura binária
 
     if (arquivo != NULL)
     {
-        Client loaded_client;
+        Client *loaded_client = malloc(sizeof(Client));
+
+        if (loaded_client == NULL)
+        {
+            // Lida com a falha de alocação de memória
+            return NULL;
+        }
 
         // Lê enquanto conseguir ler o valor do tipo Client
-        while (fread(&loaded_client, sizeof(Client), 1, arquivo) == 1)
+        while (fread(loaded_client, sizeof(Client), 1, arquivo) == 1)
         {
-            //Compara o parametro CPF com os valores dos CPFs recuperados do arquivo
-            if (strcmp(cpf, loaded_client.cpf) == 0)
+            // Compara o parametro CPF com os valores dos CPFs recuperados do arquivo
+            if (strcmp(cpf, loaded_client->cpf) == 0)
             {
-                if (silentSearch)
-                {
-                    fclose(arquivo);
-                    // cria um valor estatico para armazenar o nome do cliente
-                    static char nome_copy[99];
-                    strncpy(nome_copy, loaded_client.name, sizeof(nome_copy) - 1);
-                    nome_copy[sizeof(nome_copy) - 1] = '\0';
-                    return nome_copy;
-                }
-                else
-                {   
-                    //Exibe os valores do cliente pesquisado
-                    printf("Cliente carregado:\n");
-                    printf("Nome: %s\n", loaded_client.name);
-                    printf("Telefone: %s\n", loaded_client.fone);
-                    printf("CPF: %s\n", loaded_client.cpf);
-                    fclose(arquivo);
-                    return NULL;
-                }
-                break;
+                return loaded_client;
             }
         }
-        //Fecha o arquivo aberto
+        // Fecha o arquivo aberto
         fclose(arquivo);
         return NULL;
     }
@@ -90,6 +77,8 @@ const char *search_client_cpf(const char *cpf, bool silentSearch)
         return NULL;
     }
 }
+
+//----------------------------------------------- Reservas ------------------------------------------------------
 
 void save_file_reservation(Reservation new_reservation)
 {
@@ -109,10 +98,59 @@ void save_file_reservation(Reservation new_reservation)
     }
 }
 
-void search_reservation(const char *cpf)
+Reservation *search_reservation(const char *cpf)
 {
     // Abre o arquivo para escrita binária e define um ponteiro para o mesmo
     FILE *arquivo = fopen("reservas.db", "rb");
+
+    if (arquivo != NULL)
+    {
+        Reservation *loaded_reservation = malloc(sizeof(Reservation));
+
+        // Lê enquanto conseguir ler reservas no arquivo e o valor for diferete de 1
+        while (fread(loaded_reservation, sizeof(Reservation), 1, arquivo) == 1)
+        {
+            // Compara o valor do cpf passado como parametro com os CPFs recuperados do arquivo
+            if (strcmp(cpf, loaded_reservation->client_cpf) == 0)
+            {
+                return loaded_reservation;
+            }
+        }
+        // Fecha o arquivo aberto
+        fclose(arquivo);
+        return NULL;
+    }
+    else
+    {
+        printf("Erro ao abrir o arquivo para leitura.\n");
+        return NULL;
+    }
+}
+
+//-------------------------------------------------- Quartos ----------------------------------------------
+
+void save_file_room(Room new_room)
+{
+    // Abre o arquivo para escrita binária e define um ponteiro
+    FILE *arquivo = fopen("roons.db", "ab");
+
+    if (arquivo != NULL)
+    {
+        // Escreve um único objeto Room no arquivo
+        fwrite(&new_room, sizeof(Room), 1, arquivo);
+        // Fecha o arquivo aberto
+        fclose(arquivo);
+    }
+    else
+    {
+        printf("Erro ao abrir o arquivo para escrita.\n");
+    }
+}
+
+void search_room(const char *cpf)
+{
+    // Abre o arquivo para escrita binária e define um ponteiro para o mesmo
+    FILE *arquivo = fopen("roons.db", "rb");
 
     if (arquivo != NULL)
     {
@@ -125,11 +163,11 @@ void search_reservation(const char *cpf)
             if (strcmp(cpf, loaded_reservation.client_cpf) == 0)
             {
                 // Busca o nome do cliente ao qual o CPF esta vinculado e armazena em um ponteiro constante
-                const char *name = search_client_cpf(cpf, true);
+                const Client *copy_client = search_client_cpf(cpf);
 
-                // Exibe os dados da reserva e o nome da pessoa que reservou 
+                // Exibe os dados da reserva e o nome da pessoa que reservou
                 printf("Reserva %s:\n", loaded_reservation.number);
-                printf("Nome: %s\n", name);
+                printf("Nome: %s\n", copy_client->name);
                 printf("Dia de Entrada: %s\n", loaded_reservation.day_enter);
                 printf("Dia de Sáida: %s\n", loaded_reservation.day_exit);
             }
--- function/database_functions.h
+++ function/database_functions.h
@@ -12,6 +12,7 @@ typedef struct register_client
     char cpf[12];
     char fone[20];
 } Client;
+
 typedef struct register_reservation
 {
     char number[99];
@@ -19,10 +20,18 @@ typedef struct register_reservation
     char day_exit[99];
     char client_cpf[12];
 }Reservation;
-void add_client(Client new_client);
+
+typedef struct register_room
+{
+    char number[99];
+    char type[99];
+    char value[99];
+    char quantity[99];
+}Room;
+
 void save_file_client(Client new_client);
 void load_file_client();
-void search_reservation(const char *cpf);
+Reservation *search_reservation(const char *cpf);
 void save_file_reservation(Reservation new_reservation);
-const char *search_client_cpf(const char *cpf, bool silentSearch);
+Client *search_client_cpf(const char *cpf);
 #endif
